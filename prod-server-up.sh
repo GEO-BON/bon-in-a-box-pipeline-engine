833b9c84ad469239562f2e1b6b48057d5cf59b62
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-03 15:52:24 -0400

    WIP: server-up refers to partiaally edited compose files

--- prod-server-up.sh
+++ prod-server-up.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "Dummy test"
+echo "Updating compose file..."
+git checkout origin/repo-split -- compose.yml
+git checkout origin/repo-split -- compose.prod.yml
 
-#echo "Updating compose file..."
+echo "Pulling docker images..."
+cd .. # Back to pipeline-repo folder
+docker compose -f .server/compose.yml -f .server/compose.prod.yml -f compose.env.yml pull
 
-#echo "Pulling docker images..."
+echo "Starting the server..."
+docker compose -f .server/compose.yml -f .server/compose.prod.yml -f compose.env.yml up -d
 
-#echo "Starting the server..."
-
-#echo "Done."
+echo "Done."
