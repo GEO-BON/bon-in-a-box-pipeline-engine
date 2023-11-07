09b8b3e0e252f59c5d1033a8a805575a65559564
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-07 14:33:34 -0500

    error message instead of done, if something went wrong

--- prod-server-up.sh
+++ prod-server-up.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RED="\033[31m"
+ENDCOLOR="\033[0m"
+
 echo "Updating server files..."
 git checkout origin/repo-split -- compose.yml
 git checkout origin/repo-split -- compose.prod.yml
@@ -16,8 +19,14 @@ git checkout origin/repo-split -- script-stubs
 echo "Pulling docker images..."
 cd .. # Back to pipeline-repo folder
 docker compose -f .server/compose.yml -f .server/compose.prod.yml -f compose.env.yml pull
+if [[ $? -ne 0 ]] ; then
+    echo "${RED}Failed${ENDCOLOR}" ; exit 1
+fi
 
 echo "Starting the server..."
 docker compose -f .server/compose.yml -f .server/compose.prod.yml -f compose.env.yml up -d
+if [[ $? -ne 0 ]] ; then
+    echo "${RED}Failed${ENDCOLOR}"; exit 1
+fi
 
 echo "Done."
