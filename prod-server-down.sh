cc712a3169d332a5d5055302aab5dd7b5d11634c
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-07 15:11:09 -0500

    prod paths for server down

--- prod-server-down.sh
+++ prod-server-down.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f .server/compose.yml -f .server/compose.prod.yml -f compose.env.yml down
+docker compose -f .server/compose.yml -f .server/compose.prod.yml -f compose.env.yml --env-file .server/.prod-paths.env down
