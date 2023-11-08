f1504a6ee25f6c69e44d939b26331af8876c5aab
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-08 16:27:27 -0500

    using main branch

--- prod-server-up.sh
+++ prod-server-up.sh
@@ -15,18 +15,18 @@ assertSuccess () {
 }
 
 echo "Updating server files..."
-git checkout origin/repo-split -- .prod-paths.env ; assertSuccess
-git checkout origin/repo-split -- compose.yml ; assertSuccess
-git checkout origin/repo-split -- compose.prod.yml ; assertSuccess
-git checkout origin/repo-split -- prod-server-down.sh ; assertSuccess
+git checkout origin/main -- .prod-paths.env ; assertSuccess
+git checkout origin/main -- compose.yml ; assertSuccess
+git checkout origin/main -- compose.prod.yml ; assertSuccess
+git checkout origin/main -- prod-server-down.sh ; assertSuccess
 
-git checkout origin/repo-split -- .github/findDuplicateDescriptions.sh ; assertSuccess
-git checkout origin/repo-split -- .github/findDuplicateIds.sh ; assertSuccess
-git checkout origin/repo-split -- .github/scriptValidationSchema.yml ; assertSuccess
+git checkout origin/main -- .github/findDuplicateDescriptions.sh ; assertSuccess
+git checkout origin/main -- .github/findDuplicateIds.sh ; assertSuccess
+git checkout origin/main -- .github/scriptValidationSchema.yml ; assertSuccess
 
-git checkout origin/repo-split -- http-proxy/conf.d-prod/ngnix.conf ; assertSuccess
+git checkout origin/main -- http-proxy/conf.d-prod/ngnix.conf ; assertSuccess
 
-git checkout origin/repo-split -- script-stubs ; assertSuccess
+git checkout origin/main -- script-stubs ; assertSuccess
 
 echo "Pulling docker images..."
 cd .. # Back to pipeline-repo folder
