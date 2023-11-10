d7b428a6f15d7fc0743d8d75fb4054f4c05ec6ac
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-10 09:04:02 -0500

    branch as a param

--- prod-server-up.sh
+++ prod-server-up.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Fetches all the files necessary, then runs the production server.
 
+# Optional arg 1: branch name of server repo, default "main"
+branch=${1:-"main"}
+
 if [ -d "pipeline-repo" ]; then
   echo "ERROR: Do not run directly! This script is meant to be ran by server-up.sh in the pipeline-repo folder."
   exit 1
@@ -15,18 +18,18 @@ assertSuccess () {
 }
 
 echo "Updating server files..."
-git checkout origin/main -- .prod-paths.env ; assertSuccess
-git checkout origin/main -- compose.yml ; assertSuccess
-git checkout origin/main -- compose.prod.yml ; assertSuccess
-git checkout origin/main -- prod-server-down.sh ; assertSuccess
+git checkout origin/$branch -- .prod-paths.env ; assertSuccess
+git checkout origin/$branch -- compose.yml ; assertSuccess
+git checkout origin/$branch -- compose.prod.yml ; assertSuccess
+git checkout origin/$branch -- prod-server-down.sh ; assertSuccess
 
-git checkout origin/main -- .github/findDuplicateDescriptions.sh ; assertSuccess
-git checkout origin/main -- .github/findDuplicateIds.sh ; assertSuccess
-git checkout origin/main -- .github/scriptValidationSchema.yml ; assertSuccess
+git checkout origin/$branch -- .github/findDuplicateDescriptions.sh ; assertSuccess
+git checkout origin/$branch -- .github/findDuplicateIds.sh ; assertSuccess
+git checkout origin/$branch -- .github/scriptValidationSchema.yml ; assertSuccess
 
-git checkout origin/main -- http-proxy/conf.d-prod/ngnix.conf ; assertSuccess
+git checkout origin/$branch -- http-proxy/conf.d-prod/ngnix.conf ; assertSuccess
 
-git checkout origin/main -- script-stubs ; assertSuccess
+git checkout origin/$branch -- script-stubs ; assertSuccess
 
 echo "Pulling docker images..."
 cd .. # Back to pipeline-repo folder
