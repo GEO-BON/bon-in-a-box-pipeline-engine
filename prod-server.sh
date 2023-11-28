ae27c851b200f5784192e9702005b9bc1cf8237f
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-28 11:31:39 -0500

    Partial revert of "loading runner.env file"
    This reverts commit 8e01cf33cfba114e68eaed3e068a907116316ee4.

--- prod-server.sh
+++ prod-server.sh
@@ -115,17 +115,11 @@ function up {
     command pull ; assertSuccess
 
     echo "Starting the server..."
-    output=$(command up $@); returnCode=$?; 
-    echo "ReturnCode=$returnCode, output is $output"
-    if [[ $output == *"is already in use by container"* ]] ; then 
-    # Container conflict, perform clean and try again.
+    command up -d |& grep "is already in use by container"
+    if [[ $? -eq 0 ]] ; then # Container conflict, perform clean and try again.
         clean
         echo "Starting the server after a clean..."
-        command up $@ ; assertSuccess
-    else # No container conflict, check the return code
-        if [[ $returnCode -ne 0 ]] ; then
-            echo -e "${RED}FAILED${ENDCOLOR}" ; exit 1
-        fi
+        command up -d ; assertSuccess
     fi
 
     echo -e "${GREEN}Server is running.${ENDCOLOR}"
