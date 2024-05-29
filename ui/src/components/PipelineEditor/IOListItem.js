6cf545b4ca684009a197513a95821705995d2463
Jean-Michel <jean-michel.lord@mcgill.ca>
2024-05-29 16:43:38 -0400

    WIP #77 : Styling in progress

--- ui/src/components/PipelineEditor/IOListItem.js
+++ ui/src/components/PipelineEditor/IOListItem.js
@@ -19,27 +19,34 @@ export function IOListItem({io, id, valueEdited, setter, className}) {
         transition,
       };
 
-    return <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-        <p className={className}>
-            <ControlledTextArea className="label" keepWidth={true}
-                onBlur={e => valueEdited(e.target.value, "label", io, setter)}
-                onInput={preventNewLines}
-                defaultValue={io.label} />
+    return <div ref={setNodeRef} style={style} className="ioListItem">
+        <table>
+            <tbody>
+                <tr>
+                    <td {...attributes} {...listeners} className="draggable">⣿</td>
+                    <td className={className}>
+                        <ControlledTextArea className="label" keepWidth={true}
+                            onBlur={e => valueEdited(e.target.value, "label", io, setter)}
+                            onInput={preventNewLines}
+                            defaultValue={io.label} />
 
-            <br />
-            <ControlledTextArea className="description" keepWidth={true}
-                onBlur={e => valueEdited(e.target.value, "description", io, setter)}
-                defaultValue={io.description} />
+                        <br />
+                        <ControlledTextArea className="description" keepWidth={true}
+                            onBlur={e => valueEdited(e.target.value, "description", io, setter)}
+                            defaultValue={io.description} />
 
-            {io.type &&
-                <>
-                    <br />
-                    <span className="example-tag">Example: </span>
-                    <ScriptInput type={io.type} value={io.example} options={io.options}
-                        onValueUpdated={(value) => valueEdited(value, "example", io, setter)} />
-                </>
-            }
-        </p>
+                        {io.type &&
+                            <>
+                                <br />
+                                <span className="example-tag">Example: </span>
+                                <ScriptInput type={io.type} value={io.example} options={io.options}
+                                    onValueUpdated={(value) => valueEdited(value, "example", io, setter)} />
+                            </>
+                        }
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 }
 
