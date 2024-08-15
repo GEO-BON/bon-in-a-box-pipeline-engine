2e2164ecd0e4ea0596a566a0166f168c703b5fa0
Guillaume Larocque <glaroc@gmail.com>
2024-08-15 17:26:32 -0400

    Vite migration start

---
+++ ui/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true, // Automatically open the app in the browser
+    port: 3000,
+  },
+});
