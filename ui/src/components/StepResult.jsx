4380f863a185a4b245eaff21bd10025ea65bee81
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2024-10-09 16:59:55 -0400

    validating the outputs missing attributes

--- ui/src/components/StepResult.jsx
+++ ui/src/components/StepResult.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import RenderedCSV from './csv/RenderedCSV';
 import { FoldableOutputWithContext, FoldableOutput, FoldableOutputContextProvider } from "./FoldableOutput";
 import ReactMarkdown from 'react-markdown'
+import errorImg from "../img/error.svg";
+import warningImg from "../img/warning.svg";
 
 export function StepResult({data, sectionName, sectionMetadata, logs}) {
     const [activeRenderer, setActiveRenderer] = useState({});
@@ -184,21 +186,45 @@ export const SingleIOResult = memo(({ ioId, value, ioMetadata, componentId, sect
     }
 
     let title = ioId;
+    let icon = null;
+    let errorMsg = null;
     let description = null;
     if (ioMetadata) {
-        if (ioMetadata.label)
+        if (ioMetadata.label) {
             title = ioMetadata.label;
+        } else {
+            errorMsg = <p className="warning">No label was provided for input "{ioId}"</p>
+            icon = <img src={warningImg} alt="Warning" className="error-inline" />
+        }
 
-        if (ioMetadata.description)
+        if (ioMetadata.description) {
             description = <ReactMarkdown className="reactMarkdown outputDescription" children={ioMetadata.description} />;
+        } else {
+            errorMsg = <p className="warning">No description was provided for input "{ioId}"</p>
+            icon = <img src={warningImg} alt="Warning" className="error-inline" />
+        }
+
+        if(!ioMetadata.type) {
+            // error message taken care by children
+            icon = <img src={errorImg} alt="Error" className="error-inline" />
+        }
+
+        if(! /^[a-z]+(?:_[a-z]+)*$/.test(ioId)) {
+            errorMsg = <p className='warning'>{ioId} should be a snake_case id</p>
+            icon = <img src={warningImg} alt="Warning" className="error-inline" />
+        }
+    } else {
+        // error message taken care by children
+        icon = <img src={errorImg} alt="Error" className="error-inline" />
     }
 
     let isLink = isRelativeLink(value)
     return (
-        <FoldableOutputWithContext key={ioId} title={title} componentId={componentId}
+        <FoldableOutputWithContext key={ioId} title={title} componentId={componentId} icon={icon}
             inline={isLink && <a href={value} target="_blank" rel="noreferrer">{value}</a>}
             inlineCollapsed={!isLink && renderInline(value)}
             className="foldableOutput">
+            {errorMsg}
             {description}
             {renderContent(value)}
         </FoldableOutputWithContext>
