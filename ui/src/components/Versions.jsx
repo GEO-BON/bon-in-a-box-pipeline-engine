2ef6ce3355df9c6478573fc45d74d641ab76dc84
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2024-12-12 16:34:17 -0500

    license header example

--- ui/src/components/Versions.jsx
+++ ui/src/components/Versions.jsx
@@ -1,3 +1,21 @@
+/**
+    BON in a Box pipeline engine and modeling tool
+    Copyright (C) 2024, GEO BON
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { useEffect, useState } from "react";
 import { Spinner } from "./Spinner";
 import * as BonInABoxScriptService from "bon_in_a_box_script_service";
