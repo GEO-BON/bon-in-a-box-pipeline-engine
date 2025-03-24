516bc3401eb1fc9a437c08e97d0d4a0f23358fe3
Siva <siva_gaius@email.com>
2025-03-24 15:41:55 -0400

    Show error message in versions tab

--- ui/src/components/Versions.jsx
+++ ui/src/components/Versions.jsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from "react";
 import { Spinner } from "./Spinner";
+import { parseHttpError } from "./HttpErrors";
 import * as BonInABoxScriptService from "bon_in_a_box_script_service";
+import Alert from "@mui/material/Alert";
 
 export const api = new BonInABoxScriptService.DefaultApi();
 
 export default function Versions() {
   let [versions, setVersions] = useState();
+  let [httpError, setHttpError] = useState();
+
   useEffect(() => {
     api.getVersions((error, _, response) => {
-      if (response && response.text) setVersions(response.text);
-      else if (error) setVersions(error.toString());
-      else setVersions(null);
+      if ((response && response.text.startsWith("<html>")) || error) {
+	    setVersions(null);
+	    setHttpError(parseHttpError("Error fetching version information"));
+      
+      } else if (response && response.text) {
+	    setVersions(response.text);
+
+      } else {
+	    setVersions(null);
+	    setHttpError(parseHttpError("Error fetching version information"));
+      }
     });
   }, []);
 
   return (
     <p style={{ whiteSpace: "pre-wrap" }}>
-      {versions ? versions : <Spinner variant='light' />}
+      {versions ? versions : (<Alert severity="error">{httpError}</Alert>)}
     </p>
   );
 }
