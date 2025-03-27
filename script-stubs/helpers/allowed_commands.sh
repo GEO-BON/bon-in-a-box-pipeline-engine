92dc5f48144e7bdd1ff67d553fb48f8acfd3cc43
Jean-Michel Lord (Arbutus) <jean-michel.lord@mcgill.ca>
2025-03-27 15:48:50 +0000

    supporting if conditions in allowed commands (Discussion with Claude AI)

--- script-stubs/helpers/allowed_commands.sh
+++ script-stubs/helpers/allowed_commands.sh
@@ -1,74 +1,96 @@
 #!/bin/bash
 
-# This script checks for allowed commands in a loop per line
 THIS_SCRIPT=$(basename $0)
 
 function reject_command() {
-	echo "Command rejected by $THIS_SCRIPT: $1"
-	logger -t automation -p local0.info "Command rejected by $THIS_SCRIPT for user $USER: $1"
-	exit
+    echo "Command rejected by $THIS_SCRIPT: $1"
+    logger -t automation -p local0.info "Command rejected by $THIS_SCRIPT for user $USER: $1"
+    exit 1
 }
 
-function test_line() {
-    line=$1
-	echo $line
-	case "$line" in
+function is_safe_command() {
+    local cmd="$1"
+    local safe_patterns=(
 		# always available commands
-		ls*|cat*|cd*|echo*|uname*|id*|groups*)
-			$line
-		;;
+        "^ls"
+        "^cat"
+        "^cd"
+        "^echo"
+        "^uname"
+        "^id"
+        "^groups"
 		# file commands
-		mv*|cp*|rm*|mkdir*)
-            $line
-		;;
+        "^mv"
+        "^cp"
+        "^rm"
+        "^mkdir"
 		# python commands
-		python*|python3*|python3.6*|python3.7*|python3.8*|python3.9*|python3.10*|python3.11*|python2*|python2.7*)
-            $line
-		;;
+        "^python[0-9.]*"
 		# git
-		git*)
-            $line
-		;;
+        "^git"
 		# archiving commands
-		tar*|dar*|gzip*|zip*|bzip2*)
-            $line
-		;;
+        "^tar"
+        "^dar"
+        "^gzip"
+        "^zip"
+        "^bzip2"
 		# rsync
-		rsync*)
-            $line
-		;;
+        "^rsync"
 		# sftp and new scp
-		/usr/libexec/openssh/sftp-server*)
-            $line
-		;;
+        "^/usr/libexec/openssh/sftp-server"
 		# old scp
-		scp*)
-            $line
-		;;
+        "^scp"
 		# slurm commands
-		squeue*|scancel*|sbatch*|scontrol*|sq*)
-			$line
-		;;
+        "^squeue"
+        "^scancel"
+        "^sbatch"
+        "^scontrol"
+		"^sq"
 		# commands needed for apptainer
-		module*|apptainer*)
-			$line
-		;;
-		"")
-			echo "Discarding empty line"
-		;;
-		*)
-			reject_command "$line"
-        ;;
-	esac
+        "^module"
+        "^apptainer"
+		# conditionals
+        "^if"
+        "^fi"
+        "^else"
+        "^elif"
+        "^\\["
+        "^test"
+    )
+
+    for pattern in "${safe_patterns[@]}"; do
+        if [[ "$cmd" =~ $pattern ]]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
+function validate_complex_command() {
+    local command="$1"
+
+    while IFS= read -r line; do
+        # Trim whitespace
+        line=$(echo "$line" | xargs)
+
+        # Skip empty lines
+        [[ -z "$line" ]] && continue
+
+        # Check if each line is a safe command
+        if ! is_safe_command "$line"; then
+            reject_command "$line"
+        fi
+    done <<< "$command"
+}
 
 logger -t automation -p local0.info "Command called by $THIS_SCRIPT for user $USER: $SSH_ORIGINAL_COMMAND"
-# This condition comes from the DRAC sample. Not sure why it is there.
+
+# Check script name condition from original DRAC sample script
 if [[ "$THIS_SCRIPT" == "allowed_commands.sh" || "$THIS_SCRIPT" == "slurm_commands.sh" ]]; then
-    while IFS= read -r line; do
-        test_line "$line"
-    done <<< "$SSH_ORIGINAL_COMMAND"
+    validate_complex_command "$SSH_ORIGINAL_COMMAND"
+
+    # If validation passes, execute the command
+    bash -c "$SSH_ORIGINAL_COMMAND"
 else
     reject_command "$SSH_ORIGINAL_COMMAND"
 fi
