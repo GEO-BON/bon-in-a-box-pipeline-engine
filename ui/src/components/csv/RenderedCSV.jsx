92c44100309805d98523ee027fb536cfef09655d
Guillaume Larocque <glaroc@gmail.com>
2025-04-08 11:11:50 -0400

    GeoPackage and GEO JSON in run UI (#206)
    * UI with geopackage v1
    
    * Functioning GPKG in run ui
    
    * Remove markerclusterer for points and use GeoJSON instead
    
    * Code cleanup
    
    * Fix bounds
    
    * Display of GeoJSON and GeoPackage arrays
    
    * Fix bounds delay
    
    * Fix display on map with non WGS84 CRS
    
    ---------
    
    Co-authored-by: Jean-Michel Lord <jean-michel.lord@mcgill.ca>

--- ui/src/components/csv/RenderedCSV.jsx
+++ ui/src/components/csv/RenderedCSV.jsx
@@ -3,6 +3,7 @@ import CsvToHtmlTable, { parseCsvToRowsAndColumn } from "./CsvToHtmlTable";
 import MapResult from "../map/Map";
 import { Spinner } from "../Spinner";
 import Alert from "@mui/material/Alert";
+import { set } from "lodash";
 
 /**
  * Properties
@@ -80,54 +81,18 @@ function CsvToMap({ url, delimiter }) {
   const [error, setError] = useState();
   const [data, setData] = useState(null);
 
-  function stripQuotes(string) {
-    return string && string.startsWith('"') && string.endsWith('"')
-      ? string.slice(1, -1)
-      : string;
-  }
-
-  function readCoordinates(data, delimiter) {
-    const rowsWithColumns = parseCsvToRowsAndColumn(data, delimiter);
-    const headerRow = rowsWithColumns.splice(0, 1)[0];
-
-    const latRegEx = new RegExp("lat(itude)?", "i");
-    const latColumn = headerRow.findIndex((h) => latRegEx.test(h));
-
-    const lonRegEx = new RegExp("lon(gitude)?", "i");
-    const lonColumn = headerRow.findIndex((h) => lonRegEx.test(h));
-
-    if (latColumn === -1 || lonColumn === -1) {
-      setError(
-        "Both latitude and longitude columns must be present to display on a map."
-      );
-      return null;
-    }
-
-    return rowsWithColumns.map((row) => ({
-      pos: [row[latColumn], row[lonColumn]],
-      popup: (
-        <>
-          {headerRow.map(
-            (header, i) =>
-              header && (
-                <div key={header}>
-                  {stripQuotes(header)}: {stripQuotes(row[i])}
-                </div>
-              )
-          )}
-        </>
-      ),
-    }));
-  }
-
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.ok) return response.text();
-        else return Promise.reject("Error " + response.status);
-      })
+    CsvToGeojson(url, delimiter)
       .then((result) => {
-        setData(result);
+        if(result.error){
+          setError(result.error);
+          return;
+        }
+        if(result.features?.length==0){
+          setError("No coordinates found in this CSV file.");
+          return
+        }
+        setData(result);  
       })
       .catch((error) => {
         setError(error);
@@ -137,13 +102,114 @@ function CsvToMap({ url, delimiter }) {
   if (data || error)
     return (
       <>
-        {error && <Alert severity="error">{error}</Alert>}
+        {error && <Alert severity="error">Couldn't display table on map. Are you sure this table has spatial coordinates?</Alert>}
         {!error && data && (
-          <MapResult markers={readCoordinates(data, delimiter)} />
+          <MapResult json={data} />
         )}
       </>
     );
   else return <Spinner />;
 }
+
+export async function CsvToGeojson(
+  url,
+  delimiter,
+  crs
+) {
+  return await CsvToArray(url, delimiter).then((r) => {
+    let features = [];
+    features = r?.map((row) => {
+      if (
+        row.pos[0] > -9999999 &&
+        row.pos[0] < 99999999 &&
+        row.pos[1] > -9999999 &&
+        row.pos[1] < 99999999
+      ) {
+        if (
+          row.pos[0] < 90 &&
+          row.pos[0] > -90 &&
+          row.pos[1] < 180 &&
+          row.pos[1] > -180
+        ) {
+          // Assuming coordinates are really in lat/long.
+          crs = "EPSG:4326";
+
+          /*console.log("Using CRS", crs)
+          const coords = proj4(
+            crs,
+            "EPSG:4326",
+            row.pos.map((str) => parseFloat(str)).reverse()
+          );*/
+          return {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: row.pos.map((str) => parseFloat(str)).reverse(),
+            },
+            properties: { },
+          };
+        }
+        return
+      }
+    });
+    features = features.filter((f)=>(f))
+    if(features.length > 0){
+      features = features.slice(0, features.length - 1);
+    }
+    return { type: "FeatureCollection", features: features };
+  });
+}
+
+async function CsvToArray(url, delimiter) {
+  return fetch(url)
+    .then((response) => {
+      if (response.ok) return response.text();
+      else return Promise.reject("Error " + response.status);
+    })
+    .then((result) => {
+      return readCoordinates(result, delimiter);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function stripQuotes(string) {
+  return string && string.startsWith('"') && string.endsWith('"')
+    ? string.slice(1, -1)
+    : string;
+}
+
+function readCoordinates(data, delimiter) {
+  const rowsWithColumns = parseCsvToRowsAndColumn(data, delimiter);
+  const headerRow = rowsWithColumns.splice(0, 1)[0];
+
+  const latRegEx = new RegExp("lat(itude)?", "i");
+  const latColumn = headerRow.findIndex((h) => latRegEx.test(h));
+
+  const lonRegEx = new RegExp("lon(gitude)?", "i");
+  const lonColumn = headerRow.findIndex((h) => lonRegEx.test(h));
+
+  if (latColumn === -1 || lonColumn === -1) {
+    throw new Error("Both latitude and longitude columns must be present to display on a map.");
+  }
+
+  return rowsWithColumns.map((row) => ({
+    pos: [row[latColumn], row[lonColumn]],
+    popup: (
+      <>
+        {headerRow.map(
+          (header, i) =>
+            header && (
+              <div key={header}>
+                {stripQuotes(header)}: {stripQuotes(row[i])}
+              </div>
+            )
+        )}
+      </>
+    ),
+  }));
+}
+
 
 export default RenderedCSV;
