14e3de75540e6c1f4021f7e262e3a822ece81e8c
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2025-04-08 14:04:17 -0400

    Reset error as well when changing pipeline

--- ui/src/components/form/YAMLTextArea.jsx
+++ ui/src/components/form/YAMLTextArea.jsx
@@ -18,6 +18,8 @@ export default function YAMLTextArea({ metadata, data, setData, setValidationErr
   useEffect(() => { // Handle pipeline selection change
     if(metadataChanged) {
       setMetadataChanged(false)
+
+      setError(null)
       setTextContent(yaml.dump(data, {
         lineWidth: undefined,
         sortKeys: true,
