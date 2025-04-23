426fd01000eb3e0c8dce07d4664a437637c97b56
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2025-04-23 09:56:24 -0400

    Paging test + millisecond precision needed for tests to be correctly ordered.

--- script-server/src/main/kotlin/org/geobon/utils/SystemUtils.kt
+++ script-server/src/main/kotlin/org/geobon/utils/SystemUtils.kt
@@ -23,7 +23,7 @@ fun String.runToText(
 fun findFilesInFolderByDate(folder:File, fileName: String): List<File> {
     val process = ProcessBuilder(
         "/bin/bash", "-c",
-        "find $folder -type f -name $fileName -exec stat --format '%Y %n' {} \\; | sort -nr | cut -d' ' -f2-"
+        "find $folder -type f -name $fileName -exec stat --format '%.3Y %n' {} \\; | sort -nr | cut -d' ' -f2-"
     ).start()
     val reader = process.inputStream.bufferedReader()
 
--- script-server/src/test/kotlin/org/geobon/server/plugins/HistoryTest.kt
+++ script-server/src/test/kotlin/org/geobon/server/plugins/HistoryTest.kt
@@ -6,6 +6,7 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import org.geobon.pipeline.outputRoot
 import org.geobon.server.module
+import org.json.JSONArray
 import kotlin.test.*
 
 class HistoryTest {
@@ -72,7 +73,7 @@ class HistoryTest {
 
         client.get("/api/history?start=0&limit=5").apply {
             println(bodyAsText())
-            assertContains(bodyAsText(),  """"status":"error"""")
+            assertContains(bodyAsText(), """"status":"error"""")
         }
     }
 
@@ -100,7 +101,7 @@ class HistoryTest {
 
         client.get("/api/history?start=0&limit=5").apply {
             println(bodyAsText())
-            assertContains(bodyAsText(),  """"status":"error"""")
+            assertContains(bodyAsText(), """"status":"error"""")
         }
     }
 
@@ -127,8 +128,49 @@ class HistoryTest {
         }
 
         client.get("/api/history?start=0&limit=5").apply {
-            println(bodyAsText())
-            assertContains(bodyAsText(), """"status":"completed"""")
+            val response = bodyAsText()
+            assertContains(response, """"status":"completed"""")
+            val responseArray = JSONArray(response)
+            assertEquals(1, responseArray.length(), "Even though limit is 5, if there is only 1 result, length is 1")
+        }
+    }
+
+    @Test
+    fun givenLongHistory_whenGettingHistory_thenTruncated() = testApplication {
+        application { module() }
+
+        for(i in 0..8) {
+            client.post("/pipeline/1in1out_1step.json/run") { setBody("{helloWorld>helloPython.yml@0|some_int: $i}") }
+                .apply { assertEquals(HttpStatusCode.OK, status) }
+        }
+
+        client.get("/api/history?start=0&limit=5").apply {
+            val response = bodyAsText()
+            val responseArray = JSONArray(response)
+            assertEquals(5, responseArray.length(), "First page is full, 5 items")
+            assertContains(response, "some_int\":8")
+            assertContains(response, "some_int\":7")
+            assertContains(response, "some_int\":6")
+            assertContains(response, "some_int\":5")
+            assertContains(response, "some_int\":4")
+            assertFalse(response.contains("some_int\":3"))
+            assertFalse(response.contains("some_int\":2"))
+            assertFalse(response.contains("some_int\":1"))
+            assertFalse(response.contains("some_int\":0"))
+        }
+        client.get("/api/history?start=5&limit=5").apply {
+            val response = bodyAsText()
+            val responseArray = JSONArray(response)
+            assertEquals(4, responseArray.length(), "Second page has only 4 items")
+            assertFalse(response.contains("some_int\":8"))
+            assertFalse(response.contains("some_int\":7"))
+            assertFalse(response.contains("some_int\":6"))
+            assertFalse(response.contains("some_int\":5"))
+            assertFalse(response.contains("some_int\":4"))
+            assertContains(response, "some_int\":3")
+            assertContains(response, "some_int\":2")
+            assertContains(response, "some_int\":1")
+            assertContains(response, "some_int\":0")
         }
     }
 
