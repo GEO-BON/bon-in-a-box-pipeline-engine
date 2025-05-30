b0f22ff6be42c93327040db5e15310de38902645
Jean-Michel <jean-michel.lord@mcgill.ca>
2025-05-30 10:44:46 -0400

    better symlink setup handling

--- prod-server.sh
+++ prod-server.sh
@@ -5,13 +5,10 @@ GREEN="\033[32m"
 YELLOW="\033[33m"
 ENDCOLOR="\033[0m"
 
-# Checking that this script is ran from the .server folder, and not from the dev repo,
-# which would break the relative file path.
-(cd .. && ls "server-up.sh" 2> /dev/null 1>&2) # true if in .server folder with dev symlink
-if [[ 0 -ne $? && ! -f "server-up.sh" ]]; then
-    echo -e "${RED}ERROR: Do not run directly! This script is meant to be run by server-up.sh or validate.sh in the pipeline-repo folder.${ENDCOLOR}"
-    exit 1
-fi
+# Checking for symlink development setup
+[[ -L ".server" || -L "./pipeline-repo/.server" ]]
+symlink=$?
+if [[ $symlink -eq 0 ]] ; then echo "Detected a symlink on .server folder: this is a development setup!"; fi
 
 which docker > /dev/null
 if [[ $? -ne 0 ]] ; then
@@ -68,7 +65,11 @@ function prepareCommands {
 
     # Set the branch suffix. This allows to use a staging build.
     # We use remote.origin.fetch because of the partial checkout, see server-up.sh.
-    branch=$(git -C $SCRIPT_DIR config remote.origin.fetch | sed 's/.*remotes\/origin\///')
+    if [[ $symlink -eq 0 ]] ; then
+        branch=$(git branch --show-current) # dev setup
+    else
+        branch=$(git -C $SCRIPT_DIR config remote.origin.fetch | sed 's/.*remotes\/origin\///')
+    fi
 
     # In "dev symlink" setup, just get the regular branch
     if [[ $branch == "*" ]]; then branch=$(git branch --show-current); fi
