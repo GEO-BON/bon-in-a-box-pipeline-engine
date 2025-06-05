dcce4d9d9b7709f2d65c406a8823aa9fb4362636
Sivabalan Muthurajan <sivabalan.muthurajan@mcgill.ca>
2025-06-05 14:48:52 -0400

    Refactor change reduce to join for readability

--- ui/src/components/StepDescription.jsx
+++ ui/src/components/StepDescription.jsx
@@ -27,7 +27,7 @@ function generatePersonList(list) {
     return list.map((person, i, array) => {
         let email = person.email && <a href={'mailto:' + person.email} style={{textDecoration:'none'}}> &#9993;</a>
         let comma = (i !== array.length - 1) && ',' // Comma will be inside link but the space outside the link.
-        let role = person.role && <span> ({person.role.reduce((r1, r2) => r1 + ", " + r2)})</span>
+        let role = person.role && <span> ({person.role.join(', ')})</span>
         if (person.identifier)
             return <span key={i}>
                 <a href={person.identifier} target="_blank">
