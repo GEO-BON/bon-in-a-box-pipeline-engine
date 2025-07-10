7fc3335190a20bb0267c15716e553f73608a33d8
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2025-07-10 10:12:51 -0400

    Adjust output error messages in tests

--- script-server/src/test/kotlin/org/geobon/hpc/HPCConnectionTest.kt
+++ script-server/src/test/kotlin/org/geobon/hpc/HPCConnectionTest.kt
@@ -85,13 +85,12 @@ class HPCConnectionTest {
                 assertContains(body, """"R":{"state":"ERROR"""")
                 assertContains(body, """"Julia":{"state":"ERROR"""")
                 assertTrue(
-                    // if runners exist on the testing PC
-                    body.contains(""""message":"ssh: Could not resolve hostname hpc-name: Name or service not known""")
-                    // runners exist but not configured
-                        || body.contains(""""message":"Warning: Identity file /run/secrets/hpc_ssh_key not accessible: No such file or directory""")
-                    // otherwise
-                        || body.contains("""No such image: geobon/bon-in-a-box:runner-conda""")
-                , "Unexpected message: $body")
+                    // if biab not running
+                    body.contains("""Could not read image name for service \"biab-runner-conda\". Is the service running?""")
+                            // if biab is separately running on the testing PC
+                            || body.contains("""ssh: Could not resolve hostname hpc-name: Name or service not known"""),
+                    "Unexpected message: $body"
+                )
             }
         }
     }
@@ -107,17 +106,18 @@ class HPCConnectionTest {
 
             // Forcefully waiting because we launch the server connection on GlobalScope,
             // we cannot wait for it via join
-            waiting@ for (i:Int in 0..10) {
+            waiting@ for (i: Int in 0..10) {
                 Thread.sleep(10)
 
                 val response = client.get("/hpc/status")
                 val body = response.bodyAsText()
                 if (body.contains(""""R":{"state":"ERROR"""")
-                    && body.contains(""""Julia":{"state":"ERROR"""")) {
+                    && body.contains(""""Julia":{"state":"ERROR"""")
+                ) {
                     break@waiting
                 }
 
-                if(i == 10) {
+                if (i == 10) {
                     fail("request did not complete under 100ms")
                 }
             }
@@ -128,13 +128,12 @@ class HPCConnectionTest {
                 assertContains(body, """"R":{"state":"ERROR"""")
                 assertContains(body, """"Julia":{"state":"ERROR"""")
                 assertTrue(
-                    // if runners exist on the testing PC
-                    body.contains(""""message":"ssh: Could not resolve hostname hpc-name: Name or service not known""")
-                            // runners exist but not configured
-                            || body.contains(""""message":"Warning: Identity file /run/secrets/hpc_ssh_key not accessible: No such file or directory""")
-                            // otherwise
-                            || body.contains("""No such image: geobon/bon-in-a-box:runner-conda""")
-                    , "Unexpected message: $body")
+                    // if biab not running
+                    body.contains(""""message":"Could not read image name for service \"biab-runner-conda\". Is the service running?"""")
+                            // if biab is separately running on the testing PC
+                            || body.contains("""ssh: Could not resolve hostname hpc-name: Name or service not known"""),
+                    "Unexpected message: $body"
+                )
             }
         }
     }
