7a2015f039f6df7c2fbd5ff4460a505b60d1b482
Guillaume Larocque <glaroc@gmail.com>
2025-10-06 13:59:42 -0400

    Fix width of dialogs in pipeline editor

--- ui/src/components/PipelineEditor/ConstantNode.jsx
+++ ui/src/components/PipelineEditor/ConstantNode.jsx
@@ -1,21 +1,33 @@
-import { Handle, Position } from 'react-flow-renderer/nocss';
-import ScriptInput from '../form/ScriptInput';
+import { Handle, Position } from "react-flow-renderer/nocss";
+import ScriptInput from "../form/ScriptInput";
 
 // props content, see https://reactflow.dev/docs/api/nodes/custom-nodes/#passed-prop-types
 export default function ConstantNode({ id, data, type }) {
-
   return (
-    <div className='constant'>
+    <div className={`constant constant-${data.type}`}>
       <table>
         <tbody>
           <tr>
-            <td className='dragHandle'><p>{data.type}</p></td>
-            <td>
-              <ScriptInput id={id} type={data.type} value={data.value} options={data.options}
-              onValueUpdated={v => data.onConstantValueChange(id, v)} size="small" keepWidth={false}/>
+            <td className="dragHandle constant-name">
+              <p>{data.type}</p>
             </td>
-            <td className='dragHandle'>
-              <button className='arrowDownButton' title='options' onClick={(e) => data.onPopupMenu(e, id, type)} />
+            <td>
+              <ScriptInput
+                id={id}
+                type={data.type}
+                value={data.value}
+                options={data.options}
+                onValueUpdated={(v) => data.onConstantValueChange(id, v)}
+                size="small"
+                keepWidth={false}
+              />
+            </td>
+            <td className="dragHandle">
+              <button
+                className="arrowDownButton"
+                title="options"
+                onClick={(e) => data.onPopupMenu(e, id, type)}
+              />
             </td>
           </tr>
         </tbody>
