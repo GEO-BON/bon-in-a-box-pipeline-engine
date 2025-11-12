f92c3167775e112b6ac3f72883b3e5430fdc0bac
Sivabalan Muthurajan <sivabalan.muthurajan@mcgill.ca>
2025-11-12 14:04:56 -0500

    Change to about:blank instead of no where lese

--- ui/src/components/SpamField.jsx
+++ ui/src/components/SpamField.jsx
@@ -2,10 +2,10 @@
 export default function SpamField(props) {
   const handleChange = () => {
     // do captcha
-    window.location.href = "";
+    window.location.href = "about:blank";
   };
 
-  const hideStyle = {display: "none"};
+  const hideStyle = {display: "block"};
 
   const getRandomInt = (n) => Math.floor(Math.random() * n);
 
