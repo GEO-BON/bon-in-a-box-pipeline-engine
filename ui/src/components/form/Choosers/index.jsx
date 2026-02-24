dcc6014da3363737ce37984337d3b05f40408c46
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2026-02-24 15:44:37 -0500

    fix chooser yaml breaking description layout.
    Removes the bug component of #339

--- ui/src/components/form/Choosers/index.jsx
+++ ui/src/components/form/Choosers/index.jsx
@@ -45,7 +45,7 @@ export default function Choosers({
               </>
             )}
             {value && !isCompact && (
-              <pre style={{ maxWidth: "500px", overflowX: "scroll" }}>
+              <pre style={{ maxWidth: "330px", overflowX: "scroll" }}>
                 {yaml.dump(value)}
               </pre>
             )}
