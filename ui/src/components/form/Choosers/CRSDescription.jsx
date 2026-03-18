c7f0db61d41d73b1eccdd372ffbe5a2356acd17e
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2026-03-18 14:50:01 -0400

    formatted

--- ui/src/components/form/Choosers/CRSDescription.jsx
+++ ui/src/components/form/Choosers/CRSDescription.jsx
@@ -15,22 +15,23 @@ export default function CRSDescription({ bboxCRS }) {
         </div>
     }
     return <div>
-        <div style={{display:'flex',flexDirection: "row", width: "100%", maxWidth: "400px", fontSize: "0.9em"}}><div style={{flex: 1, maxWidth:"450px"}}>
-        <div><span className="CRSDescriptionLabel">Country:</span> <span className="CRSDescriptionValue">{bboxCRS.country?.englishName}</span></div>
-        <div><span className="CRSDescriptionLabel">Region:</span> <span className="CRSDescriptionValue">{bboxCRS.region?.regionName}</span></div>
-        <div><span className="CRSDescriptionLabel">CRS:</span> <span className="CRSDescriptionValue">{bboxCRS.CRS?.name} (EPSG:{bboxCRS.CRS?.code})</span></div>
+        <div style={{ display: 'flex', flexDirection: "row", width: "100%", maxWidth: "400px", fontSize: "0.9em" }}><div style={{ flex: 1, maxWidth: "450px" }}>
+            <div><span className="CRSDescriptionLabel">Country:</span> <span className="CRSDescriptionValue">{bboxCRS.country?.englishName}</span></div>
+            <div><span className="CRSDescriptionLabel">Region:</span> <span className="CRSDescriptionValue">{bboxCRS.region?.regionName}</span></div>
+            <div><span className="CRSDescriptionLabel">CRS:</span> <span className="CRSDescriptionValue">{bboxCRS.CRS?.name} (EPSG:{bboxCRS.CRS?.code})</span></div>
         </div>
-        <div style={{maxWidth:"450px"}}>
-        <div className="CRSDescriptionLabel" style={{flex:1}}>Bounding box:</div>
-        <ul style={{fontFamily: "monospace", listStyle: "none", paddingLeft: "0.5rem"}}>
-            <li>Minimum X: {bboxCRS.bbox[0]}</li>
-            <li>Minimum Y: {bboxCRS.bbox[1]}</li>
-            <li>Maximum X: {bboxCRS.bbox[2]}</li>
-            <li>Maximum Y: {bboxCRS.bbox[3]}</li>
-        </ul>
-        </div></div>
+            <div style={{ maxWidth: "450px" }}>
+                <div className="CRSDescriptionLabel" style={{ flex: 1 }}>Bounding box:</div>
+                <ul style={{ fontFamily: "monospace", listStyle: "none", paddingLeft: "0.5rem" }}>
+                    <li>Minimum X: {bboxCRS.bbox[0]}</li>
+                    <li>Minimum Y: {bboxCRS.bbox[1]}</li>
+                    <li>Maximum X: {bboxCRS.bbox[2]}</li>
+                    <li>Maximum Y: {bboxCRS.bbox[3]}</li>
+                </ul>
+            </div>
+        </div>
 
-        <a className="textButton" onClick={() => setViewSource(true)} style={{fontSize: "0.9em"}}>
+        <a className="textButton" onClick={() => setViewSource(true)} style={{ fontSize: "0.9em" }}>
             View Source
         </a>
     </div>
