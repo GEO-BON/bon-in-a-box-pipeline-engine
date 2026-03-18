7903a6f3e2257ebfc9155266ac43dd3cb4e6e0ef
Guillaume Larocque <glaroc@gmail.com>
2026-03-18 14:39:02 -0400

    Few fixes and more input types

--- ui/src/components/form/inputTypes.js
+++ ui/src/components/form/inputTypes.js
@@ -1,21 +1,25 @@
 export const inputTypes = (type) => {
   const types = {   
-    bboxCRS: "bouding box and CRS",
-    options: "options",
     'options[]': 'multiple options',
-    'image/tiff;application=geotiff': "geotiff",
-    'application/geo+json': "geojson",
-    'application/geopackage+sqlite3': "geopackage",
-    'application/dbf': "shapefile",
-    'text/csv': "csv",
-    'text/tab-separated-values': "tsv",
-    'text/plain': "text",
+    'image/tiff;application=geotiff': "path to geotiff file",
+    'image/tiff;application=geotiff[]': "comma separated list of paths to geotiff files",
+    'application/geo+json': "path to geojson file",
+    'application/geo+json[]': "comma separated list of paths to geojson files",
+    'application/geopackage+sqlite3': "path to geopackage file",
+    'application/geopackage+sqlite3[]': "comma separated list of paths to geopackage files",
+    'application/dbf': "path to shapefile",
+    'application/dbf[]': "comma separated list of paths to shapefiles",
+    'text/csv': "path to comma separated values file",
+    'text/csv[]': "comma separated list of paths to comma separated values files",
+    'text/tab-separated-values': "path to tab separated values file",
+    'text/tab-separated-values[]': "comma separated list of paths to tab separated values files",
+    'text/plain': "path to text file",
+    'text/plain[]': "comma separated list of paths to text files",
     'int': "integer",
     'int[]': "comma separated list of integers",
-    'text[]': 'comma separated list',
     'float': "decimal number",
-    'float[]': "comma separated list of decimal numbers",
-    boolean: 'yes/no'
+    'float[]': "comma separated list of decimal numbers",    
+    boolean: null
   }
-  return types[type]? types[type] : null;
+  return Object.keys(types).includes('[]') ? types[type] : type;
 };
