ed63c8d864ee305398afef0c1c8973393d6ef74e
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2026-04-02 16:53:25 -0400

    make sure comma-separated splits keep the object attribute this fixes a SHS display problem that is caused by R not putting an array when there is a single value.

--- viewer/src/components/Sidebar/PipelineOutput/index.tsx
+++ viewer/src/components/Sidebar/PipelineOutput/index.tsx
@@ -24,16 +24,23 @@ export function PipelineOutput(props: any) {
   } = props;
 
   const [selectedItem, setSelectedPaperItem] = useState("");
-  let outs: any = "";
+  let arrayOutputs: any = null;
   if (Array.isArray(outputObj.outputs)) {
-    outs = outputObj.outputs;
+    arrayOutputs = outputObj.outputs;
+
+  } else if (outputObj.type.includes("[]")) {
+    arrayOutputs = outputObj.outputs.split(",").map((url: any) => {
+      return { ...outputObj, url: url };
+    });
   }
-  if (outputObj.outputs.includes(",") && outputObj.type.includes("[]")) {
-    outs = outputObj.outputs.split(",");
+
+  // Fold array of 1 into single value
+  if (arrayOutputs.length === 1) {
+    arrayOutputs = arrayOutputs[0];
   }
-  if (outs.length === 1) {
-    outs = outs[0];
-  }
+  console.log("OUTPUT OBJ", outputObj, arrayOutputs,
+  !Array.isArray(arrayOutputs), outputObj?.type.startsWith("image/"),
+            "type" in outputObj);
 
   const handleSelect = (value: string) => {
     setSelectedOutput(value);
@@ -63,7 +70,7 @@ export function PipelineOutput(props: any) {
                 outputObj.description.slice(1)}
             </Markdown>
           </div>
-          {Array.isArray(outs) && outputObj?.type?.includes("tif") && (
+          {Array.isArray(arrayOutputs) && outputObj?.type?.includes("tif") && (
             <FormControl
               variant="standard"
               sx={{
@@ -81,7 +88,7 @@ export function PipelineOutput(props: any) {
                 onChange={(event: any) => handleSelect(event.target.value)}
                 label="Layer"
               >
-                {outs.map((o: any) => (
+                {arrayOutputs.map((o: any) => (
                   <CustomMenuItem key={`it-${o.url}`} value={o}>
                     {o?.description}
                   </CustomMenuItem>
@@ -109,17 +116,17 @@ export function PipelineOutput(props: any) {
               )}
             </FormControl>
           )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             "type" in outputObj &&
             outputObj?.type?.includes("tif") && (
               <>
                 <CustomButtonGreen
-                  key={`but-${outs.band_id}`}
+                  key={`but-${arrayOutputs.band_id}`}
                   onClick={(event: any) => {
                     handleClick(
                       event,
-                      { url: outs.url, band_id: outs.band_id },
-                      outs.type
+                      { url: arrayOutputs.url, band_id: arrayOutputs.band_id },
+                      arrayOutputs.type
                     );
                   }}
                 >
@@ -130,14 +137,14 @@ export function PipelineOutput(props: any) {
                     sx={{
                       display: "inline",
                     }}
-                    onClick={() => generateStats(outs)}
+                    onClick={() => generateStats(arrayOutputs)}
                   >
                     <BarChartIcon />
                   </CustomButton>
                 )}
               </>
             )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             (outputObj?.type?.includes("value") ||
               outputObj?.type?.includes("tsv") ||
               outputObj?.type?.includes("csv")) &&
@@ -170,7 +177,7 @@ export function PipelineOutput(props: any) {
                 </CustomButtonGreen>
               </>
             )}
-          {Array.isArray(outs) &&
+          {Array.isArray(arrayOutputs) &&
             (outputObj?.type?.includes("value") ||
               outputObj?.type?.includes("tsv") ||
               outputObj?.type?.includes("csv")) &&
@@ -192,7 +199,7 @@ export function PipelineOutput(props: any) {
                   onChange={(event: any) => handleSelect(event.target.value)}
                   label="Table"
                 >
-                  {outs.map((o: any) => (
+                  {arrayOutputs.map((o: any) => (
                     <CustomMenuItem key={`it-${o.url}`} value={o}>
                       {o.split("/").pop()}
                     </CustomMenuItem>
@@ -210,15 +217,8 @@ export function PipelineOutput(props: any) {
                 </Grid>
               </FormControl>
             )}
-          {!Array.isArray(outs) &&
-            [
-              "image/png",
-              "image/jpeg",
-              "image/jpg",
-              "image/svg",
-              "image/gif",
-              "image/bmp",
-            ].includes(outputObj?.type) &&
+          {!Array.isArray(arrayOutputs) &&
+            outputObj?.type.startsWith("image/") &&
             "type" in outputObj && (
               <CustomButtonGreen
                 key={`but-${outputObj.outputs}`}
@@ -229,7 +229,7 @@ export function PipelineOutput(props: any) {
                 See image
               </CustomButtonGreen>
             )}
-          {Array.isArray(outs) &&
+          {Array.isArray(arrayOutputs) &&
             [
               "image/png[]",
               "image/jpeg[]",
@@ -256,7 +256,7 @@ export function PipelineOutput(props: any) {
                   onChange={(event: any) => handleSelect(event.target.value)}
                   label="Layer"
                 >
-                  {outs.map((o: any) => (
+                  {arrayOutputs.map((o: any) => (
                     <CustomMenuItem key={`it-${o.url}`} value={o}>
                       {o.split("/").pop()}
                     </CustomMenuItem>
@@ -281,7 +281,7 @@ export function PipelineOutput(props: any) {
             outputObj.type == "text[]") && (
             <Typography color="secondary.light">{outputObj.outputs}</Typography>
           )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             (outputObj?.type?.includes("int") ||
               outputObj?.type?.includes("float")) &&
             "type" in outputObj && (
@@ -289,7 +289,7 @@ export function PipelineOutput(props: any) {
                 {outputObj.outputs}
               </Typography>
             )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             outputObj?.type?.includes("geo+json") &&
             "type" in outputObj && (
               <>
@@ -303,7 +303,7 @@ export function PipelineOutput(props: any) {
                 </CustomButtonGreen>
               </>
             )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             outputObj?.type?.includes("geopackage") &&
             "type" in outputObj && (
               <>
@@ -317,7 +317,7 @@ export function PipelineOutput(props: any) {
                 </CustomButtonGreen>
               </>
             )}
-          {Array.isArray(outs) &&
+          {Array.isArray(arrayOutputs) &&
             outputObj?.type?.includes("geopackage") &&
             "type" in outputObj && (
               <FormControl
@@ -337,7 +337,7 @@ export function PipelineOutput(props: any) {
                   onChange={(event: any) => handleSelect(event.target.value)}
                   label="Layer"
                 >
-                  {outs.map((o: any) => (
+                  {arrayOutputs.map((o: any) => (
                     <CustomMenuItem key={`it-${o.url}`} value={o}>
                       {o.split("/").pop()}
                     </CustomMenuItem>
@@ -355,7 +355,7 @@ export function PipelineOutput(props: any) {
                 </Grid>
               </FormControl>
             )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             outputObj?.type?.includes("json") &&
             !outputObj?.type?.includes("geo+json") &&
             "type" in outputObj && (
@@ -368,7 +368,7 @@ export function PipelineOutput(props: any) {
                 See results
               </CustomButtonGreen>
             )}
-          {!Array.isArray(outs) &&
+          {!Array.isArray(arrayOutputs) &&
             outputObj?.type?.includes("text/html") &&
             "type" in outputObj && (
               <CustomButtonGreen key={`but-${outputObj.outputs}`}>
