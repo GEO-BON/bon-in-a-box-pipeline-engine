52ab3f1e768d722f16fec49daa342350f471cb52
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2026-04-08 14:03:39 -0400

    Rename + checking internal "country" object

--- script-server/src/test/resources/scripts/assertCountry.py
+++ script-server/src/test/resources/scripts/assertCountry.py
@@ -2,27 +2,29 @@ import sys, json
 
 # Reading inputs
 data = biab_inputs()
-some_country = data['some_country']
+some_input = data['some_input']
+print(f"Input received: {some_input}")
 
 print("Is valid json?")
-if not isinstance(some_country, dict) :
+if not isinstance(some_input, dict) :
     print("No.")
     biab_error_stop('This is not a json object')
 
 print("Yes!")
 
 print("Is valid country?")
+country = some_input["country"]
 required_keys = {"ISO3", "bboxWGS84", "englishName"}
 
-if not required_keys.issubset(some_country):
+if not required_keys.issubset(country):
     print("No.")
     biab_error_stop("Country object must contain ISO3, bboxWGS84 and englishName")
 
-if not isinstance(some_country["ISO3"], str) or len(some_country["ISO3"]) != 3:
+if not isinstance(country["ISO3"], str) or len(country["ISO3"]) != 3:
     print("No.")
     biab_error_stop("ISO3 must be a 3-letter string")
 
-bbox = some_country["bboxWGS84"]
+bbox = country["bboxWGS84"]
 if (
     not isinstance(bbox, list)
     or len(bbox) != 4
@@ -31,11 +33,11 @@ if (
     print("No.")
     biab_error_stop("bboxWGS84 must be a list of 4 numbers")
 
-if not isinstance(some_country["englishName"], str) or not some_country["englishName"].strip():
+if not isinstance(country["englishName"], str) or not country["englishName"].strip():
     print("No.")
     biab_error_stop("englishName must be a non-empty string")
 
 print("Yes!")
 
 # Write outputs
-biab_output('the_same', some_country)
+biab_output('the_same', some_input)
