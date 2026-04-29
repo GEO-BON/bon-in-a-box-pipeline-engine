4a4df3a9b8ae68e70ce48e28caab8ffd36fd7b92
Jean-Michel Lord (arbutus) <jean-michel.lord@mcgill.ca>
2026-04-29 20:44:50 +0000

    error formatting ctnd

--- script-server/src/main/kotlin/org/geobon/k8s/ApiException.kt
+++ script-server/src/main/kotlin/org/geobon/k8s/ApiException.kt
@@ -4,13 +4,16 @@ import io.kubernetes.client.openapi.ApiException
 import org.json.JSONObject
 
 fun ApiException.toFormattedString() : String {
-    var formatted = "Kubernetes API error ($code): $message"
+    var formatted = "Kubernetes API error ($code): "
 
     if(!responseBody.isNullOrBlank()) {
         val json = JSONObject(responseBody)
-        json.remove("message")
+        formatted += json.optString("message", "") + "\n"
         json.remove("code")
-        formatted += "\n${json.toString(2)}"
+        json.remove("message")
+        formatted += json.toString(2)
+    } else {
+        formatted += message
     }
 
     return formatted
