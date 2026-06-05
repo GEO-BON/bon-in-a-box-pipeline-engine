2b158882d94ea6bc99330da78cf6f8b4a7e4c6d9
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2026-06-05 12:17:36 -0400

    fix cp path error

--- script-stubs/system/condaPackEnvironment.sh
+++ script-stubs/system/condaPackEnvironment.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 condaEnvName=$1
 condaPackDir=$2
-condaEnvFile="/conda-env-yml/$condaEnvName.yml"
 
 # Conda-pack the environment if a folder was supplied.
 if [[ -d "$condaPackDir" ]]; then
     zip=$condaPackDir/$condaEnvName.tar.gz
 
-    if [ !-f "$zip" ]; then
+    if [ -f "$zip" ]; then
         echo "Conda-pack archive $zip already packed."
         exit 0
     fi
@@ -15,7 +14,10 @@ if [[ -d "$condaPackDir" ]]; then
     echo "Packing conda environment $condaEnvName."
     mamba activate base
     conda-pack --n-threads -1 --quiet -n $condaEnvName -o $zip
-    cp condaEnvFile $condaPackDir/$condaEnvName.yml
+
+    condaEnvFile="/conda-env-yml/$condaEnvName.yml"
+    cp $condaEnvFile $condaPackDir/$condaEnvName.yml
+    
     echo "Conda environment packed to $zip using conda-pack!"
     mamba deactivate # base
 fi
