58090ba60d2dcbf33a8cb253bbde065e2247f6b8
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2026-07-08 10:33:17 -0400

    Fixed a few quirks with geopackage class: - better variable names - retry fitBound more than once - closing geopackage after all layers read. - adding feature group to map

--- ui/src/components/map/GeoPackageLayer.jsx
+++ ui/src/components/map/GeoPackageLayer.jsx
@@ -10,51 +10,61 @@ import sqlWasmUrl from "@ngageoint/geopackage/dist/sql-wasm.wasm?url";
  *
  * @param props
  */
-function GeoPackageLayer(props) {
-  const { geoPackage, setGeoPackage } = props;
-  const map = useMap();
+function GeoPackageLayer({ geoPackage, setGeoPackage }) {
 
+  const map = useMap();
   const fg = L.featureGroup();
 
   const loadGeoPackage = async (geoPackage) => {
     try {
       const geoP = await GeoPackageAPI.open(geoPackage);
-      const layers = geoP.getFeatureTables();
-      layers.forEach((ly) => {
-        const l = L.geoPackageFeatureLayer([], {
+      const layerNames = geoP.getFeatureTables();
+      layerNames.forEach((layerName) => {
+        const layer = L.geoPackageFeatureLayer([], {
           geoPackageUrl: geoPackage,
-          layerName: ly,
+          layerName: layerName,
           attribution: "BON in a Box",
         });
-        l.addTo(fg);
-        l.addTo(map);
-        geoP.close();
+        layer.addTo(fg);
       });
+      geoP.close();
+      fg.addTo(map);
       return fg;
+
     } catch (error) {
       console.error("Error loading GeoPackage:", error);
+      return false;
     }
-    return false;
   };
 
   useEffect(() => {
+    let fitBoundsTimeoutId;
     let ignore = false;
     setSqljsWasmLocateFile((file) => sqlWasmUrl);
     if (geoPackage !== "" && map) {
-      loadGeoPackage(geoPackage).then((f) => {
-        setTimeout(() => { //Necessary to wait for the layer to load before setting the bounds
-          if (ignore) return;
-          const bounds = L.latLngBounds(f.getBounds());
-          if (bounds.isValid()) {
-            map.fitBounds(bounds);
-          }
-        }, 500);
+      loadGeoPackage(geoPackage).then((featureGroup) => {
+        if(featureGroup) {
+          var n = 0;
+          const fitBoundsTimer = () => { // Necessary to wait for the layer to load before setting the bounds
+            if (ignore) return;
+
+            const bounds = L.latLngBounds(featureGroup.getBounds());
+            if (bounds.isValid()) {
+              map.fitBounds(bounds);
+            } else if (n < 20) {
+              n++;
+              fitBoundsTimeoutId = setTimeout(fitBoundsTimer, 250);
+            }
+          };
+          fitBoundsTimeoutId = setTimeout(fitBoundsTimer, 250);
+        }
       });
     }
     return () => {
       setGeoPackage("");
       fg.remove();
       ignore = true;
+      clearTimeout(fitBoundsTimeoutId);
     };
   }, [geoPackage, map]);
 
