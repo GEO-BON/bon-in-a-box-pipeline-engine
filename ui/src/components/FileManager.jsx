3d06b4d68c0fd06bc2e7efb07bf4cac14890dcb5
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2026-08-03 13:05:07 -0400

    added backend functionalities
    using SVAR's Filemanager Data Provider and Go backend structure

--- ui/src/components/FileManager.jsx
+++ ui/src/components/FileManager.jsx
@@ -1,12 +1,34 @@
+import { useState, useEffect } from 'react';
+import { RestDataProvider } from "@svar-ui/filemanager-data-provider";
 import { Filemanager, WillowDark } from "@svar-ui/react-filemanager";
 import "@svar-ui/react-filemanager/all.css";
 import "./FileManager.css";
 
+const BackendServer = "http://localhost:3200";
+const restProvider = new RestDataProvider(BackendServer);
+
+
 export default function FileManager() {
+    const [data, setData] = useState([]);
+    const [drive, setDrive] = useState({});
+
+    const init = (api) => {
+        api.setNext(restProvider); // wires save/rename/delete/upload to the backend
+    };
+
+    useEffect(() => {
+        Promise.all([restProvider.loadFiles(), restProvider.loadInfo()]).then(
+            ([files, info]) => {
+                setData(files);
+                setDrive(info.stats);
+            }
+        );
+    }, []);
+
     return (
         <div className="file-manager">
             <WillowDark>
-                <Filemanager api={"http://localhost:3200"} />
+                <Filemanager init={init} data={data} drive={drive} />
             </WillowDark>
         </div>
     );
